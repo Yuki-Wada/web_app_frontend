9e9d47d3aca386f2a86dd46f612275ff831c73bb
Yuki Wada <yuki.w.228285@gmail.com>
2021-12-11 22:51:12 +0900

    modify python cloud run url

--- src/constant.js
+++ src/constant.js
@@ -1,11 +1,7 @@
 const HOST = (process.env.NODE_ENV === 'production') ?
-    'webappbackend-norahk2d5a-uw.a.run.app':
-    // 'pythonapi-dot-third-upgrade-315803.uw.r.appspot.com' :
-
+    'webappbackend-norahk2d5a-uk.a.run.app':
     'localhost:8888';
-    // 'webappbackend-norahk2d5a-uw.a.run.app';
-    // 'pythonapi-dot-third-upgrade-315803.uw.r.appspot.com';
-    // 'intense-chamber-41417.herokuapp.com';
+    // 'webappbackend-norahk2d5a-uk.a.run.app';
 
 export default Object.freeze({
     HOST: HOST,
